efd06e6ecc8b9ebe7162e6edd4d5bee2fa10fecd
oktarian <rian15194@gmail.com>
2025-08-30 02:20:00 +0700

    psakdos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
 <body>
 
     <header class="header">
+        adawdasdajjd
         <div class="container">
             <nav class="navbar">
                 <a href="#" class="logo">THE AKAZA RETURN</a>
